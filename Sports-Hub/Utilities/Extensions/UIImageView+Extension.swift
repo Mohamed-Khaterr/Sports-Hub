53334bfe8efcc0cae1c822c99cb9cd77ad4ad94b
Mohamed Khater <mohamed.15751@gmail.com>
2023-09-28 06:34:48 -0700

    Comment some code in UIImageView Extension

--- Sports-Hub/Utilities/Extensions/UIImageView+Extension.swift
+++ Sports-Hub/Utilities/Extensions/UIImageView+Extension.swift
@@ -10,17 +10,24 @@ import Kingfisher
 
 extension UIImageView {
     func setImage(withURL url: URL?){
+        self.kf.setImage(with: url, placeholder: UIImage(named: "placeholderImage"))
+        /*
         let selectedContentMode = self.contentMode
         let selectedBackgroundColor = self.backgroundColor
         self.contentMode = .scaleAspectFit
         self.backgroundColor = .white
         self.kf.setImage(with: url, placeholder: UIImage(named: "placeholderImage")) { [weak self] _ in
-            self?.alpha = 0
-            UIView.animate(withDuration: 2.5) {
-                self?.alpha = 1
-                self?.contentMode = selectedContentMode
-                self?.backgroundColor = selectedBackgroundColor
+            self?.contentMode = selectedContentMode
+            self?.backgroundColor = selectedBackgroundColor
+            UIView.animate(withDuration: 0.5) {
+                self?.alpha = 0
+                
+            } completion: { _ in
+                UIView.animate(withDuration: 2.5) {
+                    self?.alpha = 1
+                }
             }
         }
+        */
     }
 }
