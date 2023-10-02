42b174c0c61a3c62c7ba4e2f78b8bcd2d316a212
Mohamed Khater <mohamed.15751@gmail.com>
2023-10-01 20:49:59 -0700

    Create AllSportsAPIEndPoint protocol

---
+++ Sports-Hub/Services/Network/AllSportsAPIEndPoint.swift
@@ -0,0 +1,15 @@
+//
+//  AllSportsAPIEndPoint.swift
+//  Sports-Hub
+//
+//  Created by Khater on 01/10/2023.
+//
+
+import Foundation
+
+
+protocol AllSportsAPIEndPoint {
+    var path: String { get }
+    var met: String { get }
+    var params: [String: Any?] { get }
+}
