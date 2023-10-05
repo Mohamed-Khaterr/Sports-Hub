b59ca3fad493ae2c21906cecab45bf3664e3c4d6
Mohamed Khater <mohamed.15751@gmail.com>
2023-10-05 15:42:02 -0700

    Create SportType enum

---
+++ Sports-Hub/Models/SportType.swift
@@ -0,0 +1,15 @@
+//
+//  SportType.swift
+//  Sports-Hub
+//
+//  Created by Khater on 05/10/2023.
+//
+
+import Foundation
+
+
+enum SportType {
+    case football
+    case basketball
+    case cricket
+}
