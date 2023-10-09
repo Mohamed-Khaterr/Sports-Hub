f277fee4f66f2de71bcbb478f0095164a74019aa
Mohamed Khater <mohamed.15751@gmail.com>
2023-10-08 20:40:27 -0700

    Add and remove league from favourties

--- Sports-Hub/Scenes/LeagueDetails/View/LeagueDetailsViewController.swift
+++ Sports-Hub/Scenes/LeagueDetails/View/LeagueDetailsViewController.swift
@@ -20,42 +20,43 @@ class LeagueDetailsViewController: UIViewController {
     
     // MARK: - Properties
     let viewModel = LeagueDetailsViewModel()
-    private var isFavorite = false
-    private var favBarButtonImageName: String {
-        return isFavorite ? "heart.fill" : "heart"
-    }
+    private var isFavourite = false
 
     // MARK: LifeCycel
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupNavigationBar()
-        setupCollectionView()
         bind()
+        viewModel.fetchLeague()
         viewModel.fetchUpcomingEvents()
         viewModel.fetchLatestEvents()
         viewModel.fetchLeagueTeams()
+        
+        setupNavigationBar()
+        setupCollectionView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        viewModel.fetchLeagueFromDB()
     }
     
     // MARK: - NavigationBar
     private func setupNavigationBar() {
         title = "League Details"
-        let favBarButton = UIBarButtonItem(image: UIImage(systemName: favBarButtonImageName), style: .done, target: self, action: #selector(favoriteButtonPressed))
+        let favBarButton = UIBarButtonItem(image: nil, style: .done, target: self, action: #selector(favoriteButtonPressed))
         favBarButton.tintColor = .label
         navigationItem.rightBarButtonItem = favBarButton
     }
     
     // MARK: - Favorite Button
     @objc private func favoriteButtonPressed() {
-        isFavorite = !isFavorite
-        navigationItem.rightBarButtonItem?.image = UIImage(systemName: favBarButtonImageName)
-        navigationItem.rightBarButtonItem?.tintColor = isFavorite ? .red : .label
-        if isFavorite {
-            favoriteAnimation.start()
-        } else {
+        if isFavourite {
+            viewModel.removeLeagueFromFavourites()
             unFavoriteAnimation.start()
+        } else {
+            viewModel.addLeagueToFavourites()
+            favoriteAnimation.start()
         }
-        
-        // TODO: Save to CoreData
     }
     
     
@@ -170,6 +171,13 @@ class LeagueDetailsViewController: UIViewController {
             teamVC.setTeamID(teamID)
             teamVC.setSportType(sportType)
             self?.navigationController?.pushViewController(teamVC, animated: true)
+        }
+        
+        viewModel.isFavouriteLeague = { [weak self] isFavourite in
+            self?.isFavourite = isFavourite
+            let imageName = isFavourite ? "heart.fill" : "heart"
+            self?.navigationItem.rightBarButtonItem?.image = UIImage(systemName: imageName)
+            self?.navigationItem.rightBarButtonItem?.tintColor = isFavourite ? .red : .label
         }
     }
 }
